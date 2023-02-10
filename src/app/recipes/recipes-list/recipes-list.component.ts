e206bf8458142062229c1bdcd9cda21b1a4ca810
Davidkuepo <davidkuepo83@gmail.com>
2023-02-10 17:31:13 +0100

    mon deuxieme commit

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter,Output } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -7,8 +7,16 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
+
  recipes: Recipe[] =[
   new Recipe('name of a recipe','description of the recipe','https://sf2.viepratique.fr/wp-content/uploads/sites/2/2015/03/Tagliatelles-au-bacon-Recette-TagliatelleChampignon.jpg'),
   new Recipe('name of a recipe','description of the recipe','https://sf2.viepratique.fr/wp-content/uploads/sites/2/2015/03/Tagliatelles-au-bacon-Recette-TagliatelleChampignon.jpg'),
  ];
+
+
+ onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
+
 }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient{
+  public Name: String;
+  public amount: Number;
+
+  constructor(name:string,amount:number){
+     this.Name = name;
+     this.amount = amount
+  }
+
+}
